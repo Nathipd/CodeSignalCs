a12b45442ebc8c6376633142bedee62e2b008e78
Nathalia <nathalia.desenv@gmail.com>
2023-08-01 10:58:27 -0300

    Create repository and first project
    Signed-off-by: Nathalia <nathalia.desenv@gmail.com>

---
+++ intro/01TheJourneyBegins/add/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//Write a function that returns the sum of two numbers.
+
+namespace Add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution(1, 2);
+        }
+
+        static int Solution(int param1, int param2)
+        {
+
+             int _sum = param1 + param2;
+
+            Console.Write($"{param1} + {param2} = {_sum}");
+
+            return _sum;
+
+        }
+    }
+}
