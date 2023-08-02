2e644a3f0657414fa1f5d3250a2b05ef6dbead52
Nathalia <nathalia.desenv@gmail.com>
2023-08-02 10:39:45 -0300

    New project AdjacentElementsProduct
    Signed-off-by: Nathalia <nathalia.desenv@gmail.com>

---
+++ intro/02EdgeOfTheOcean/AdjacentElementsProduct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+namespace AdjacentElementsProduct
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            Solution(new int[] {3, 6, -2, -5, 7, 3});
+        }
+
+        static int Solution(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            return 0;
+        }
+
+    }
+}
