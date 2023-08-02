6a787c84885ac4b36ca6189a11ba69c340ca6cd4
Nathalia <nathalia.desenv@gmail.com>
2023-08-02 10:45:31 -0300

    Add ShapeArea project
    Signed-off-by: Nathalia <nathalia.desenv@gmail.com>

---
+++ intro/02EdgeOfTheOcean/ShapeArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side.
+//Your task is to find the area of a polygon for a given n.
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution(4);
+        }
+
+        static int Solution(int n)
+        {
+
+            int area = (n * n) + ((n - 1) * (n - 1));
+
+            Console.WriteLine($"The area is {area}");
+            return area;
+        }
+
+    }
+}
