ad8d3e4d46fd65e06d5d815ea12f6d207486e55e
Nathalia <nathalia.desenv@gmail.com>
2023-08-02 10:55:30 -0300

    Add Statues project
    Signed-off-by: Nathalia <nathalia.desenv@gmail.com>

---
+++ intro/02EdgeOfTheOcean/Statues/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+//Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect,
+//he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that.
+//Help him figure out the minimum number of additional statues needed.
+
+namespace Statues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution(new int[] { 6, 2, 3, 8 });
+        }
+
+        static int Solution(int[] statues)
+        {
+            int _arrayLenght = statues.Length;
+            int _biggerValue = statues.Max();
+            int _ciggerValue = statues.Min();
+            int _remainingStatues = 0;
+
+            for (int _currentNumber = _ciggerValue; _currentNumber <= _biggerValue; _currentNumber++)
+            {
+                bool _exist = false;
+
+                for (int i = 0; i < _arrayLenght; i++)
+                {
+                    if (_currentNumber == statues[i])
+                    {
+                        Console.WriteLine(statues[i]);
+                        _exist = true;
+
+                        break;
+                    }
+                }
+                if (!_exist)
+                {
+                    _remainingStatues++;
+                }
+            }
+
+            return _remainingStatues;
+        }
+
+    }
+}
