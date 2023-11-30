b8050930f7950a51e7425710c14b241f4ba422f3
Nathalia <nathalia.desenv@gmail.com>
2023-11-29 21:46:30 -0300

    Finish the challenge
    Signed-off-by: Nathalia <nathalia.desenv@gmail.com>

--- intro/02EdgeOfTheOcean/AdjacentElementsProduct/Program.cs
+++ intro/02EdgeOfTheOcean/AdjacentElementsProduct/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 // Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
 
@@ -6,18 +7,38 @@ namespace AdjacentElementsProduct
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
 
-            Solution(new int[] {3, 6, -2, -5, 7, 3});
+            Solution(new int[] { 3, 6, -2, -5, 7, 3 });
+
         }
 
-        static int Solution(int[] inputArray)
+        static int Solution(int[] inputarray)
         {
-            for (int i = 0; i < inputArray.Length; i++)
+            int _inputLength = inputarray.Length;
+
+            int _biggersProduct = int.MinValue;
+
+            for (int i = 0; i < _inputLength -1; i++)
             {
-                Console.WriteLine(i);
+
+                int _currentIndex = i;
+                int _nextIndex = _currentIndex + 1;
+
+                int _currentElement = inputarray[_currentIndex];
+                int _nextElement = inputarray[_nextIndex];
+
+                int _product = _currentElement * _nextElement;
+
+                if (_product > _biggersProduct)
+                {
+                   _biggersProduct = _product;
+                }
+
             }
+
+            Console.WriteLine(_biggersProduct);
 
             return 0;
         }
