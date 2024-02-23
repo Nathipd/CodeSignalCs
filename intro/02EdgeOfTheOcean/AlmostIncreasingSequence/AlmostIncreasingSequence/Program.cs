6c081fbbf13be25cfeb8546117b60a004c12ab51
Nathi <nathaliapiresdias0@gmail.com>
2024-02-23 19:30:51 -0300

    Create project
    Signed-off-by: Nathi <nathaliapiresdias0@gmail.com>

---
+++ intro/02EdgeOfTheOcean/AlmostIncreasingSequence/AlmostIncreasingSequence/Program.cs
@@ -0,0 +1,29 @@
+﻿// Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by 
+// removing no more than one element from the array.
+
+//Example
+
+    //For sequence = [1, 3, 2, 1], the output should be
+    //solution(sequence) = false.
+
+    //There is no one element in this array that can be removed in order to get a strictly increasing sequence.
+
+//Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one 
+//element is also considered to be strictly increasing.
+
+using System;
+
+namespace AlmostIncreasing
+{
+    class Program
+    {
+static void Main (string[] args){
+
+}
+
+static bool Solution(int[] sequence) {
+
+return true;
+}
+    }
+}
